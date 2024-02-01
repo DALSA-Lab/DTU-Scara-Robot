6c267bbd9c8ef61ed2f3700ed8976a42ba620308
Karol <s213030@dtu.dk>
2024-02-01 17:28:17 +0100

    IK, FK

--- Raspberry/FK.py
+++ Raspberry/FK.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from sympy import pi, Matrix, symbols, cos, sin
-#test
 
 def scara_FK(theta_1, d_1, theta_3, theta_4):
 
@@ -12,8 +11,7 @@ def scara_FK(theta_1, d_1, theta_3, theta_4):
     theta_4_rad = math.radians(theta_4)
     d_1 = d_1 + 123 #BASE OFFSET
 
-
-    # Define values for the symbolic variables
+    
     a_1_val, alpha_1_val, d_1_val, theta_1_val = 0, 0, d_1, -theta_1_rad+pi/2
     a_2_val, alpha_2_val, d_2_val, theta_2_val = 329.498, 0, 0, 0
     a_3_val, alpha_3_val, d_3_val, theta_3_val = 285.0, pi, 0, -theta_3_rad
@@ -54,8 +52,6 @@ def scara_FK(theta_1, d_1, theta_3, theta_4):
     H_dh_03 = H_dh_1 * H_dh_2 * H_dh_3
     H_dh_04 = H_dh_1 * H_dh_2 * H_dh_3 * H_dh_4
 
-    # print(H_dh_04)
-
     x = H_dh_04[0, 3]
     y = H_dh_04[1, 3]
     z = H_dh_04[2, 3]
@@ -64,62 +60,11 @@ def scara_FK(theta_1, d_1, theta_3, theta_4):
 
     return x, y, z, g
 
-    print("x:", x)
-    print("y:", y)
-    print("z:", z)
-
-    link1_x = [0, H_dh_01[0,3]]
-    link1_y = [0, H_dh_01[1,3]]
-    link1_z = [0, H_dh_01[2,3]]
-
-    link2_x = [H_dh_01[0,3], H_dh_02[0,3]]
-    link2_y = [H_dh_01[1,3], H_dh_02[1,3]]
-    link2_z = [H_dh_01[2,3], H_dh_02[2,3]]
-
-    link3_x = [H_dh_02[0,3], H_dh_03[0,3]]
-    link3_y = [H_dh_02[1,3], H_dh_03[1,3]]
-    link3_z = [H_dh_02[2,3], H_dh_03[2,3]]
-
-    link4_x_v1 = [H_dh_03[0,3], H_dh_03[0,3]]
-    link4_y_v1 = [H_dh_03[1,3], H_dh_03[1,3]]
-    link4_z_v1 = [H_dh_03[2,3], H_dh_03[2,3]-102.0]
-
-    link4_x_v2 = [H_dh_03[0,3], H_dh_04[0,3]]
-    link4_y_v2 = [H_dh_03[1,3], H_dh_04[1,3]]
-    link4_z_v2 = [H_dh_03[2,3]-102.0, H_dh_04[2,3]]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    # Plot the line segments
-    ax.plot(link1_x, link1_y, link1_z, 'b-', linewidth=20)
-    ax.plot(link2_x, link2_y, link2_z, 'b-', linewidth=10)
-    ax.plot(link3_x, link3_y, link3_z, 'b-', linewidth=5)
-    ax.plot(link4_x_v1, link4_y_v1, link4_z_v1, 'b-', linewidth=2)
-    ax.plot(link4_x_v2, link4_y_v2, link4_z_v2, 'b-', linewidth=2)
-    ax.plot(x, y, z, 'b-', linewidth=5)
-
-    # Set axis limits
-    ax.set_xlim([-700, 700])
-    ax.set_ylim([-700, 700])
-    ax.set_zlim([0, 500])
-
-    # Set axis labels
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-
-    # Show the plot
-    plt.show()
 
 
 
 
 
 
-x,y,z,theta_4 = scara_FK(0, 0, 0, 0)
 
-print("X:",x)
-print("Y:",y)
-print("Z:",z)
-print("THETA4:",theta_4)
+
--- Raspberry/IK.py
+++ Raspberry/IK.py
@@ -1,31 +1,25 @@
 import math
 
 def calculate_scara_angles(x, y, z, gripper_or, pos):
-    l_1 = 329.5
-    l_2 = 285.0
+    l_1 = 329.5 #First arm link
+    l_2 = 285.0 #Second arm link
+    d_b = 123.0 #Base offset
+
+    #Change according to desired TCP
     l_3 = 25.65 # TCP vertical offset
     l_4 = 102.0 # TCP horizontal offset
-    # l_3 = 0.0 # TCP vertical offset
-    # l_4 = 0.0 # TCP horizontal offset
-    d_b = 123.0
-
-    print("X coord:", x)
-    print("Y coord:", y)
-    print("Z coord:", z)
-    print("A coord:", gripper_or)
-    print("POS: ", pos)
+    # l_3 = 110.0 # TCP vertical offset
 
     gripper_or_radians = math.radians(gripper_or)
     
-    #TCP change
+    #From TCP to J4
     d_x = l_3*math.sin(gripper_or_radians)
     x = x - d_x
-
     d_y = l_3*math.cos(gripper_or_radians)
     y = y - d_y
-
     z = z + l_4 - d_b
   
+    
     d = math.sqrt(x**2 + y**2)
     if d > 614.5+l_4:
         return None, float('inf')  # Position out of bounds, infinite cost
@@ -34,25 +28,21 @@ def calculate_scara_angles(x, y, z, gripper_or, pos):
     s2_v1 = -math.sqrt(1 - c2**2)
     s2_v2 = math.sqrt(1 - c2**2)
 
-    theta_2_v1 = math.atan2(s2_v1, c2)
-    theta_2_v2 = math.atan2(s2_v2, c2)
-
+# First solution
     theta_1_v1 = math.atan2(x, y) - math.atan2(l_2 * s2_v1, l_1 + l_2 * c2)
-    theta_1_v2 = math.atan2(x, y) - math.atan2(l_2 * s2_v2, l_1 + l_2 * c2)
-
+    theta_2_v1 = math.atan2(s2_v1, c2)
     theta_1_v1_deg = math.degrees(theta_1_v1)
     theta_2_v1_deg = math.degrees(theta_2_v1)
 
+#Second solution
+    theta_1_v2 = math.atan2(x, y) - math.atan2(l_2 * s2_v2, l_1 + l_2 * c2)
+    theta_2_v2 = math.atan2(s2_v2, c2)
     theta_1_v2_deg = math.degrees(theta_1_v2)
     theta_2_v2_deg = math.degrees(theta_2_v2)
 
     solutions = []
-    # print("THETA1 v1:", theta_1_v1_deg)
-    # print("THETA2 v1:", theta_2_v1_deg)
 
-    # print("THETA1 v2:", theta_1_v2_deg)
-    # print("THETA2 v2:", theta_2_v2_deg)
-
+#Check if J3 fits within range
     if -150 < theta_2_v1_deg < 150:
         solutions.append((theta_1_v1_deg, theta_2_v1_deg))
 
@@ -65,15 +55,15 @@ def calculate_scara_angles(x, y, z, gripper_or, pos):
 
     for solution in solutions:
         cost = abs(solution[0] - pos[0]) + abs(solution[1] - pos[2])
+        #Choose solution (currently IK chooses the left position if solutions have the same cost)
         if cost < min_cost or (cost == min_cost and solution[0] < best_solution[0]):
             min_cost = cost
             best_solution = solution
 
-    theta_1 = round(best_solution[0], 2)  # Use best_solution instead of solution
     d = round(z, 2)
-    theta_3 = round(best_solution[1], 2)  # Use best_solution instead of solution
-    
-    # alpha = theta_1 + theta_3 + theta_4
+    theta_1 = round(best_solution[0], 2)
+    theta_3 = round(best_solution[1], 2)
+
     theta_4 = gripper_or - theta_1 - theta_3
     theta_4 = round(theta_4, 2)
 
@@ -83,28 +73,4 @@ def calculate_scara_angles(x, y, z, gripper_or, pos):
 
 
 
-# # Example usage:
-# x_input = float(input("Enter the value for x: "))
-# y_input = float(input("Enter the value for y: "))
-# z_input = float(input("Enter the value for z: "))
-# a_input = float(input("Enter the value for gripper orientation: "))
-# speed_input = float(input("Enter the speed: "))
-# pos_input = [float(input("Enter the current value for j1: ")),
-#              float(input("Enter the current value for j2: ")),
-#              float(input("Enter the current value for j3: ")),
-#              float(input("Enter the current value for j4: "))]
-
-# theta_1, d, theta_3, theta_4 = calculate_scara_angles(x_input, y_input, z_input, a_input, pos_input)
-
-# print("THETA_1: ", theta_1)
-# print("D: ", d)
-# print("THETA_3: ", theta_3)
-# print("THETA_4: ", theta_4)
-
-
-# theta_1, d_1, theta_3, theta_4 = calculate_scara_angles(-500, -300, 300, -90, [400, 400, 200, 0])
-# print("THETA1:", theta_1)
-# print("D2:", d_1)
-# print("THETA3:", theta_3)
-# print("THETA4:", theta_4)
 
