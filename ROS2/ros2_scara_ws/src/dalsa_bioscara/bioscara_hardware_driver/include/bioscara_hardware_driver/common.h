4188e040d3c87f3d1876d06940eb642b40f1b9ec
sbstorz <s.storz01@gmail.com>
2025-11-04 18:38:58 +0100

    add error macro (not used)

--- ROS2/ros2_scara_ws/src/dalsa_bioscara/bioscara_hardware_driver/include/bioscara_hardware_driver/common.h
+++ ROS2/ros2_scara_ws/src/dalsa_bioscara/bioscara_hardware_driver/include/bioscara_hardware_driver/common.h
@@ -4,17 +4,16 @@
  * @brief A file containing utility macros and functions
  * @version 0.1
  * @date 2025-05-27
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
 #ifndef COMMON_H
 #define COMMON_H
 
-
 /**
  * @brief Macro to dump a buffer to cout
- * 
+ *
  * @param buffer pointer to a buffer to dump to the console
  * @param size number of bytes to dump
  */
@@ -27,5 +26,20 @@
     }                                 \
     std::cout << std::endl;           \
   }
+
+  /**
+   * @brief Macro which executes a function and returns from the calling function with the error code if the called function fails.
+   * 
+   * Adapted from the [ESP-IDF](https://github.com/espressif/esp-idf/blob/ff97953b32a32e44f507593320b50d728eea3f06/components/esp_common/include/esp_check.h#L19)
+   */
+#define RETURN_ON_ERROR(x)     \
+  do                           \
+  {                            \
+    int err_rc_ = (x);         \
+    if (unlikely(err_rc_ < 0)) \
+    {                          \
+      return err_rc_;          \
+    }                          \
+  } while (0)
 
 #endif // COMMON_H
